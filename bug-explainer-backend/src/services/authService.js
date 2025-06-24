cc8efb70e77fd72761aa414102e2156fe2bc3948
Sushwetabm <114595484+Sushwetabm@users.noreply.github.com>
2025-06-24 14:22:12 +0530

    Added the forgot password mail functionality

--- bug-explainer-backend/src/services/authService.js
+++ bug-explainer-backend/src/services/authService.js
@@ -6,6 +6,7 @@ const User = require("../models/User");
 const crypto = require("crypto");
 const Token = require("../models/Token");
 const bcrypt = require("bcrypt");
+const sendEmail = require("../utils/email");
 
 const register = async (userBody) => {
   if (await User.isEmailTaken(userBody.email)) {
@@ -42,7 +43,7 @@ const forgotPassword = async (email) => {
     .update(resetToken)
     .digest("hex");
 
-  await Token.findOneAndDelete({ user: user._id }); // One token per user
+  await Token.findOneAndDelete({ user: user._id });
 
   await Token.create({
     user: user._id,
@@ -50,12 +51,20 @@ const forgotPassword = async (email) => {
     expiresAt: new Date(Date.now() + 15 * 60 * 1000), // 15 minutes
   });
 
-  // Simulate sending reset link
   const resetLink = `http://localhost:3000/reset-password?token=${resetToken}`;
+
+  const htmlMessage = `
+    <h2>Password Reset Request</h2>
+    <p>Hello ${user.name || "User"},</p>
+    <p>You requested to reset your password.</p>
+    <p><a href="${resetLink}" target="_blank">Click here to reset your password</a></p>
+    <p>This link is valid for 15 minutes.</p>
+  `;
+
+  await sendEmail(user.email, "Reset your password", htmlMessage);
+
   return resetLink;
 };
-
-// Reset Password
 const resetPassword = async (token, newPassword) => {
   const hashedToken = crypto.createHash("sha256").update(token).digest("hex");
 
@@ -74,6 +83,7 @@ const resetPassword = async (token, newPassword) => {
 
   await Token.deleteOne({ _id: resetTokenDoc._id }); // Remove used token
 };
+
 const generateAuthTokens = (user) => {
   const accessToken = jwt.sign({ id: user.id }, config.jwt.secret, {
     expiresIn: config.jwt.expiresIn,
