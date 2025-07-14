ab969fabc67a60834ace85dd20efe6f79534a0dd
Sushwetabm <114595484+Sushwetabm@users.noreply.github.com>
2025-07-14 15:52:25 +0530

    updates index.js in routes

--- bug-explainer-backend/src/routes/index.js
+++ bug-explainer-backend/src/routes/index.js
@@ -1,15 +1,28 @@
+// const express = require("express");
+// const authRoutes = require("./auth");
+// const analysisRoutes = require("./analysis");
+// const userRoutes = require("./user");
+// const auth = require("../middleware/auth");
+
+// const router = express.Router();
+
+// router.use("/auth", authRoutes);
+// // router.use("/analysis", [auth(), analysisRoutes]);
+// // router.use("/user", [auth(), userRoutes]);
+// router.use("/user", auth(), userRoutes);
+// router.use("/analysis", auth(), analysisRoutes);
+
+// module.exports = router;
+
 const express = require("express");
 const authRoutes = require("./auth");
 const analysisRoutes = require("./analysis");
 const userRoutes = require("./user");
-const auth = require("../middleware/auth");
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
-// router.use("/analysis", [auth(), analysisRoutes]);
-// router.use("/user", [auth(), userRoutes]);
-router.use("/user", auth(), userRoutes);
-router.use("/analysis", auth(), analysisRoutes);
+router.use("/user", userRoutes);
+router.use("/analysis", analysisRoutes);
 
 module.exports = router;
