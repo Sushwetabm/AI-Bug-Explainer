1dbcb23b40872897c5848dfc17642a938326db39
Sushwetabm <114595484+Sushwetabm@users.noreply.github.com>
2025-07-15 00:10:29 +0530

    added console logs in authService.js

--- bug-explainer-backend/src/services/authService.js
+++ bug-explainer-backend/src/services/authService.js
@@ -93,6 +93,9 @@ const forgotPassword = async (email) => {
   `;
 
   try {
+    console.log("EMAIL_FROM:", process.env.EMAIL_FROM);
+    console.log("EMAIL_APP_PASSWORD:", process.env.EMAIL_APP_PASSWORD);
+
     await sendEmail(user.email, "Reset your password", htmlMessage);
   } catch (err) {
     console.error("❌ Failed to send email:", err); // 👈 log this
